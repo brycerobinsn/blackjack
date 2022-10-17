2e3f4d48b8a1e34bb4fcc2dcfafc5a30856ec2b3
brycerobinsn <bryce.robinson@gmail.com>
2022-10-17 14:20:50 -0700

    player object begin deal function

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
 console.log('hello world')
+// select elements
+
+
+
+// variable declarations
+const player = {
+    hand: [],
+    chipCount: 0
+}
+const dealerHand = []
+
+
+// functions
+function dealCards (user, dealer) {
+    console.log('dealing hand')
+}
+// event listeners
