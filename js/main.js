0619b1c875cdde39c1d864b41cd7cc81eff9e2c3
brycerobinsn <bryce.robinson@gmail.com>
2022-10-17 16:47:28 -0700

    add deck as array of objects

--- js/main.js
+++ js/main.js
@@ -9,6 +9,268 @@ const player = {
     chipCount: 0
 }
 const dealerHand = []
+const deck = [{
+        value: 11,
+        suit: 'Spade',
+        img: 'url here'
+
+    },
+    {
+        value: 2,
+        suit:'Spade',
+        img: 'url here'
+    },
+    {
+        value: 3,
+        suit:'Spade',
+        img: 'url here'
+    },
+    {
+        value: 4,
+        suit:'Spade',
+        img: 'url here'
+    },
+    {
+        value: 5,
+        suit:'Spade',
+        img: 'url here'
+    },
+    {
+        value: 6,
+        suit:'Spade',
+        img: 'url here'
+    },
+    {
+        value: 7,
+        suit:'Spade',
+        img: 'url here'
+    },
+    {
+        value: 8,
+        suit:'Spade',
+        img: 'url here'
+    },
+    {
+        value: 9,
+        suit:'Spade',
+        img: 'url here'
+    },
+    {
+        value: 10,
+        suit:'Spade',
+        img: 'url here'
+    },
+    {
+        value: 10,
+        suit:'Spade',
+        img: 'url here'
+    },
+    {
+        value: 10,
+        suit:'Spade',
+        img: 'url here'
+    },
+    {
+        value: 10,
+        suit:'Spade',
+        img: 'url here'
+    },
+    {
+        value: 11,
+        suit: 'Club',
+        img: 'url here'
+    },
+    {
+        value: 2,
+        suit: 'Club',
+        img: 'url here'
+    },
+    {
+        value: 3,
+        suit: 'Club',
+        img: 'url here'
+    },
+    {
+        value: 4,
+        suit: 'Club',
+        img: 'url here'
+    },
+    {
+        value: 5,
+        suit: 'Club',
+        img: 'url here'
+    },
+    {
+        value: 6,
+        suit: 'Club',
+        img: 'url here'
+    },
+    {
+        value: 7,
+        suit: 'Club',
+        img: 'url here'
+    },
+    {
+        value: 8,
+        suit: 'Club',
+        img: 'url here'
+    },
+    {
+        value: 9,
+        suit: 'Club',
+        img: 'url here'
+    },
+    {
+        value: 10,
+        suit: 'Club',
+        img: 'url here'
+    },
+    {
+        value: 10,
+        suit: 'Club',
+        img: 'url here'
+    },
+    {
+        value: 10,
+        suit: 'Club',
+        img: 'url here'
+    },
+    {
+        value: 10,
+        suit: 'Club',
+        img: 'url here'
+    },
+    {
+        value: 1,
+        suit: 'Diamond',
+        img: 'url here'
+    },
+    {
+        value: 2,
+        suit: 'Diamond',
+        img: 'url here'
+    },
+    {
+        value: 3,
+        suit: 'Diamond',
+        img: 'url here'
+    },
+    {
+        value: 4,
+        suit: 'Diamond',
+        img: 'url here'
+    },
+    {
+        value: 5,
+        suit: 'Diamond',
+        img: 'url here'
+    },
+    {
+        value: 6,
+        suit: 'Diamond',
+        img: 'url here'
+    },
+    {
+        value: 7,
+        suit: 'Diamond',
+        img: 'url here'
+    },
+    {
+        value: 8,
+        suit: 'Diamond',
+        img: 'url here'
+    },
+    {
+        value: 9,
+        suit: 'Diamond',
+        img: 'url here'
+    },
+    {
+        value: 10,
+        suit: 'Diamond',
+        img: 'url here'
+    },
+    {
+        value: 10,
+        suit: 'Diamond',
+        img: 'url here'
+    },
+    {
+        value: 10,
+        suit: 'Diamond',
+        img: 'url here'
+    },
+    {
+        value: 10,
+        suit: 'Diamond',
+        img: 'url here'
+    },
+    {
+        value: 1,
+        suit: 'Heart',
+        img: 'url here'
+    },
+    {
+        value: 2,
+        suit: 'Heart',
+        img: 'url here'
+    },
+    {
+        value: 3,
+        suit: 'Heart',
+        img: 'url here'
+    },
+    {
+        value: 4,
+        suit: 'Heart',
+        img: 'url here'
+    },
+    {
+        value: 5,
+        suit: 'Heart',
+        img: 'url here'
+    },
+    {
+        value: 6,
+        suit: 'Heart',
+        img: 'url here'
+    },
+    {
+        value: 7,
+        suit: 'Heart',
+        img: 'url here'
+    },
+    {
+        value: 8,
+        suit: 'Heart',
+        img: 'url here'
+    },
+    {
+        value: 9,
+        suit: 'Heart',
+        img: 'url here'
+    },
+    {
+        value: 10,
+        suit: 'Heart',
+        img: 'url here'
+    },
+    {
+        value: 10,
+        suit: 'Heart',
+        img: 'url here'
+    },
+    {
+        value: 10,
+        suit: 'Heart',
+        img: 'url here'
+    },
+    {
+        value: 10,
+        suit: 'Heart',
+        img: 'url here'
+    },
+]
 
 
 // functions
@@ -18,14 +280,21 @@ function randomCard (min, max) {
     max = Math.floor(max)
     return Math.floor(Math.random() * (max - min + 1) + min)
 }
-
+// assign cards to each player alternating 
 function dealCards () {
     player.hand.push(randomCard(1,13))
+    console.log(`Player hand: ${player.hand}`)
+    console.log(`Dealer has: ${dealerHand}`)
     dealerHand.push(randomCard(1,13))
+    console.log(`Player hand: ${player.hand}`)
+    console.log(`Dealer has: ${dealerHand}`)
     player.hand.push(randomCard(1,13))
+    console.log(`Player hand: ${player.hand}`)
+    console.log(`Dealer has: ${dealerHand}`)
     dealerHand.push(randomCard(1,13))
     console.log(`Player hand: ${player.hand}`)
     console.log(`Dealer has: ${dealerHand}`)
 }
 dealCards()
+
 // event listeners
