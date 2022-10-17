7027af4eeb7ac81df69c7a84a0578cff2a4b32e4
brycerobinsn <bryce.robinson@gmail.com>
2022-10-17 14:32:37 -0700

    basic deal function

--- js/main.js
+++ js/main.js
@@ -12,7 +12,20 @@ const dealerHand = []
 
 
 // functions
-function dealCards (user, dealer) {
-    console.log('dealing hand')
+// random number to find card value
+function randomCard (min, max) {
+    min = Math.ceil(min)
+    max = Math.floor(max)
+    return Math.floor(Math.random() * (max - min + 1) + min)
 }
+
+function dealCards () {
+    player.hand.push(randomCard(1,13))
+    dealerHand.push(randomCard(1,13))
+    player.hand.push(randomCard(1,13))
+    dealerHand.push(randomCard(1,13))
+    console.log(`Player hand: ${player.hand}`)
+    console.log(`Dealer has: ${dealerHand}`)
+}
+dealCards()
 // event listeners
